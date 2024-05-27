e85faf39af512cd089689ff2a0d1500963c16a1e
donits09 <98079068+donits09@users.noreply.github.com>
2024-05-27 13:44:03 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('date.timezone','Asia/Manila');
+date_default_timezone_set('Asia/Manila');
+
+
+$dsn = "PostgreSQL30"; // Replace with your DSN name
+$user = "postgres";    // Replace with your database username
+$pass = "admin12345";    // Replace with your database password
+
+$conn = odbc_connect($dsn, $user, $pass);
+
+if (!$conn) {
+    die("Connection failed: " . odbc_errormsg());
+}
+
+date_default_timezone_set('Asia/Manila');
+
+?>
+
+
