38dc71b2ef87850193f6ed7a1fe3c62397b6de87
donits09 <98079068+donits09@users.noreply.github.com>
2024-07-15 14:17:13 +0800

    update

--- admin/modals/main_modals.php
+++ admin/modals/main_modals.php
@@ -131,6 +131,19 @@
     </div>
   </div>
 </div>
+<div class="modal fade" id="previewCarModal" tabindex="-1" role="dialog" aria-labelledby="previewCarModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="previewCarModalLabel">CAR Preview</h5>
+            </div>
+            <div class="modal-body" onclick="closePreviewModal()">
+                <iframe id="previewCarIframe" style="width: 100%; height: 500px;" frameborder="0"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 
 
