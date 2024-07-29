b36ad58d8f4df59d65e0873b9dc2e828a9a8a6c8
galawangjudz <jaevoli@gmail.com>
2024-07-29 16:13:14 +0800

    add

--- viewer/atap/fetch_atap_list.php
+++ viewer/atap/fetch_atap_list.php
@@ -124,6 +124,7 @@ if (!empty($account_no)) {
                         <a class="dropdown-item view_atap" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>" data-no="<?php echo $row['c_atap_no'] ?>">
                             <span class="fa fa-eye text-primary"></span> View
                         </a>
+                        <?php if ($row['c_encoded_by'] == $username){ ?>
                         <div class="dropdown-divider <?php echo ($row['status'] != 0) ? 'd-none' : ''; ?>"></div>
                         <a class="dropdown-item edit_atap_spec <?php echo ($row['status'] != 0) ? 'd-none' : ''; ?>" href="javascript:void(0)" 
                             data-acc-no="<?php echo $row['c_account_no']; ?>"
@@ -135,6 +136,7 @@ if (!empty($account_no)) {
                         <a class="dropdown-item delete_data <?php echo ($row['status'] != 0) ? 'd-none' : ''; ?>" href="javascript:void(0)" data-id="<?php echo $row['id']; ?>" data-no="<?php echo $row['c_atap_no']; ?>">
                             <span class="fa fa-ban text-danger"></span> Cancel
                         </a>
+                        <?php } ?>
                     </div>
                 </td>
             </tr>
