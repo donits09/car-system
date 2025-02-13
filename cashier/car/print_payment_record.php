37bc7a0ef53b90e49bfd9fe9047243b61e0815e4
galawangjudz <jaevoli@gmail.com>
2025-02-13 15:19:50 +0800

    print payment record

--- cashier/car/print_payment_record.php
+++ cashier/car/print_payment_record.php
@@ -118,20 +118,23 @@ include('../../inc/header.php');?>
                                     <th style="padding-left:5px; width:150px;">Fixed Factor : </th><td><?php echo $row1['c_fixed_factor'];?></td>
                                 </tr>
                                 <tr>
-                                    <th style="padding-left:5px; width:150px;">Total C.Price: </th><td><?php echo number_format($row1['c_tcp'],2);?></td>
+                                    <th style="padding-left:5px; width:150px;">TCP Discount Amt: </th><td><?php echo number_format($row1['c_tcp_discount_amount'],2);?></td>
                                     <th style="padding-left:5px; width:150px;">(<?php echo $row1['c_down_percent'];?>%) DP Amt. : </th><td><?php echo number_format((($row1['c_net_tcp'] * ($row1['c_down_percent']/100)) - $row1['c_reservation']),2);?></td>
                                     <th style="padding-left:5px; width:150px;">Terms to Pay : </th><td><?php echo $row1['c_terms'];?> mos.</td>
                                 </tr>
                                 <tr>
-                                    <th style="padding-left:5px; width:150px;">VAT: </th><td><?php echo number_format($row1['c_vat_amount'],2);?></td>
+                                    <th style="padding-left:5px; width:150px;">Total C.Price: </th><td><?php echo number_format($row1['c_tcp'],2);?></td>
                                     <th style="padding-left:5px; width:150px;">DP / Month : </th><td><?php echo number_format($row1['c_monthly_down'],2);?></td>
                                     <th style="padding-left:5px; width:150px;">Monthly Amort  : </th><td> (<?php echo $row1['c_interest_rate'];?>%) <?php echo number_format ($row1['c_monthly_payment'],2);?></td>
                                 </tr>
                                 <tr>
-                                    <th style="padding-left:5px; width:150px;">NET T.C.Price : </th><td><?php echo number_format($row1['c_net_tcp'],2);?></td>
+                                    <th style="padding-left:5px; width:150px;">VAT: </th><td><?php echo number_format($row1['c_vat_amount'],2);?></td>
                                     <th style="padding-left:5px; width:150px;">House Model : </th><td><?php echo $row1['c_house_model'];?></td>
                                     <th style="padding-left:5px; width:150px;">Commencing on : </th><td><?php echo  $row1['c_start_date'];?></td>
-                                </tr>   
+                                </tr> 
+                                <tr> 
+                                <th style="padding-left:5px; width:150px;">NET T.C.Price : </th><td><?php echo number_format($row1['c_net_tcp'],2);?></td>
+                                </tr> 
                             </table>
                         </div>
                     </div>
