6e7983c0ef49e192c57bd097aa5e4e71ffc69a71
Edhen Sese <edhensese28@gmail.com>
2025-05-27 08:32:10 +0800

    CAR/Update/carlist

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 ini_set('date.timezone','Asia/Manila');
 date_default_timezone_set('Asia/Manila');
 
-define('base_url','http://localhost/car/');
+define('base_url','http://192.168.0.111/car/');
 
 global $dsn, $user, $pass;
 $dsn = "PostgreSQL30"; 
@@ -21,9 +21,9 @@ function redirect($url) {
 }
 
 /* FOR REMARKS ONLY PROBLEM THE SIZE ON odbc_connect*/
-$dbhost = 'localhost';
+$dbhost = '192.168.0.123';
 $dbport = '5432';
-$dbname = 'CAR_TESTDB';
+$dbname = 'ProductionDB';
 $dbuser = 'postgres';
 $dbpass = 'admin12345';
 
@@ -34,9 +34,9 @@ if (!$pg_conn) {
 
 
 ##### JUDZ CONNECTION ######
-$dbhost = 'localhost';
+$dbhost = '192.168.0.123';
 $dbport = '5432'; 
-$dbname = 'CAR_TESTDB';
+$dbname = 'ProductionDB';
 $dbuser = 'postgres';
 $dbpass = 'admin12345';
 
