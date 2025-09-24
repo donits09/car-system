55f2bf3888b8b728f2aebd8857b5151a4a1a64b7
galawangjudz <jaevoli@gmail.com>
2025-09-24 15:25:26 +0800

    update 20250924

--- initialize.php
+++ initialize.php
@@ -2,7 +2,7 @@
 if(!defined('base_url')) define('base_url','http://192.168.0.111/car/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 
-if(!defined('DB_SERVER')) define('DB_SERVER',"ProductionDB");
+if(!defined('DB_SERVER')) define('DB_SERVER',"192.168.0.123");
 
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"postgres");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"admin12345");
